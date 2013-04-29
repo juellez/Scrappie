99a40c92da5eedcb301043cd563a26b60e5b8aee
jewel mlnarik <jewel.m@gmail.com>
2013-04-28 23:42:52 -0700

    Abstract Google UA Code
    pull google analytics tracking into config

--- index.php
+++ index.php
@@ -98,7 +98,7 @@ $hashtag = get_hashtag();
 			  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 			  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 			  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-			  ga('create', 'UA-7919133-3', 'scrappie.us');
+			  ga('create', '<?php echo GOOGLE_ANALYTICS_UA ?>', '<?php echo $config['domain'] ?>');
 			  ga('send', 'pageview');
 		});
 
